a3c73e5366791a3d5bd85ae1c527414346a806b3
alfathaulia <alfath.go.hype@gmail.com>
2021-02-15 18:57:29 +0700

    .

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-ini/ini v1.62.0 // indirect
 	github.com/google/uuid v1.1.1
+	github.com/minio/minio-go v6.0.14+incompatible
 	github.com/minio/minio-go/v7 v7.0.8
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.20.12
--- uploader.go
+++ uploader.go
@@ -1,7 +1,10 @@
 package upload
 
 import (
+	"bytes"
 	"fmt"
+	"log"
+	urs "net/url"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -10,6 +13,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -48,20 +53,20 @@ func Register(base *gin.RouterGroup, u *Uploader) {
 
 //Upload file
 func (u *Uploader) Upload(c *gin.Context) (file *File, err error) {
-	// endpoint := "storage.mojavstudio.com"
-	// accessKeyID := "@bc12345"
-	// secretAccessKey := "@bc123456"
-	// useSSL := true
+	endpoint := "xx.xx.com"
+	accessKeyID := "@xxxx"
+	secretAccessKey := "@xxxx"
+	useSSL := true
 
-	// // Initialize minio client object.
-	// minioClient, err := minio.New(endpoint, &minio.Options{
-	// 	Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-	// 	Secure: useSSL,
-	// })
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// log.Printf("%s\n", minioClient.EndpointURL())
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("%s\n", minioClient.EndpointURL())
 
 	f, err := c.FormFile("file")
 	if err != nil {
@@ -87,30 +92,30 @@ func (u *Uploader) Upload(c *gin.Context) (file *File, err error) {
 	}
 	path := filepath.Join(dir, storedFileName)
 
-	// userMetaData := map[string]string{"x-amz-acl": "public-read"}
-	// cacheControl := "max-age=31536000"
-	// buffer := make([]byte, f.Size)
-	// fileBytes := bytes.NewReader(buffer)
-	// n, err := minioClient.PutObject(c, "hipam-app", f.Filename, fileBytes, f.Size, minio.PutObjectOptions{ContentType: f.Header.Get("Content-Type"), CacheControl: cacheControl, UserMetadata: userMetaData})
-	// // Set request parameters
-	// reqParams := make(urs.Values)
-	// reqParams.Set("response-content-disposition", "attachment; filename=\"your-filename.txt\"")
-	// object, err := minioClient.PresignedGetObject(c, "hipam-app", f.Filename, time.Second*24*60*60, reqParams)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// fmt.Println("Successfully get bytes: ", object)
-	// fmt.Println("Successfully uploaded bytes: ", n)
+	userMetaData := map[string]string{"x-amz-acl": "public-read"}
+	cacheControl := "max-age=31536000"
+	buffer := make([]byte, f.Size)
+	fileBytes := bytes.NewReader(buffer)
+	n, err := minioClient.PutObject(c, "hipam-app", f.Filename, fileBytes, f.Size, minio.PutObjectOptions{ContentType: f.Header.Get("Content-Type"), CacheControl: cacheControl, UserMetadata: userMetaData})
+	// Set request parameters
+	reqParams := make(urs.Values)
+	reqParams.Set("response-content-disposition", "attachment; filename=\"your-filename.txt\"")
+	object, err := minioClient.PresignedGetObject(c, "hipam-app", f.Filename, time.Second*24*60*60, reqParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully get bytes: ", object)
+	fmt.Println("Successfully uploaded bytes: ", n)
 	file = &File{
-		ID:       id,
-		Filename: f.Filename,
-		Size:     f.Size,
-		MIMEType: f.Header.Get("Content-Type"),
-		Path:     path,
-		Ext:      ext,
-		URL:      url("/", u.StaticRoot, path),
-		// CloudStorage: object.Path,
+		ID:           id,
+		Filename:     f.Filename,
+		Size:         f.Size,
+		MIMEType:     f.Header.Get("Content-Type"),
+		Path:         path,
+		Ext:          ext,
+		URL:          url("/", u.StaticRoot, path),
+		CloudStorage: object.Path,
 	}
 	err = u.DB.Create(file).Error
 	if err != nil {
